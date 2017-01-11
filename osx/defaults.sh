47fdf9c92da9069f6612d5b5591f1e5af6b4b7ca
Lukas Kämmerling <lukas.kaemmerling@s-imk.de>
2017-01-11 22:31:36 +0100

    Add more Functional for my Mac

---
+++ osx/defaults.sh
@@ -0,0 +1,9 @@
+sudo -v
+
+# Finder: show hidden files by default
+defaults write com.apple.finder AppleShowAllFiles -bool true
+
+# Finder: show all filename extensions
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
+killall Finder
